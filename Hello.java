4c7678f8e679169989128c066e9ca1a28d43b169
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-19 20:56:47 +0530

    update

--- Hello.java
+++ Hello.java
@@ -5,15 +5,51 @@ class Hello {
         // int sum = num1 + num2;
         // System.out.println(sum);
 
-        float marks1 = 5.6f;
-        double marks2 = 5.6;
-        int num = 343;
-        byte num3 = 127;
-        long num5 = 949499444;
-        short sh = 9999;
+        // float marks1 = 5.6f;
+        // double marks2 = 5.6;
+        // int num = 343;
+        // byte num3 = 127;
+        // long num5 = 949499444;
+        // short sh = 9999;
 
-        System.out.println(marks1);
-        System.out.println(marks2);
+        // boolean num = 1;  //error 
+        // boolean num3 = true; 
+
+         // Integer Literals
+         int decimal = 100;
+         int hex = 0x64;
+         int octal = 0144;
+         int binary = 0b1100100;
+         
+         // Floating-Point Literals
+         float floatNumber = 3.14f;
+         double doubleNumber = 2.71828;
+         
+         // Character Literals
+         char letter = 'A';
+         char unicodeChar = '\u0041';
+         
+         // String Literal
+         String greeting = "Hello, World!";
+         
+         // Boolean Literal
+         boolean isJavaFun = true;
+         
+         // Null Literal
+         String str = null;
+         
+         // Printing literals
+         System.out.println("Decimal: " + decimal);
+         System.out.println("Hexadecimal: " + hex);
+         System.out.println("Octal: " + octal);
+         System.out.println("Binary: " + binary);
+         System.out.println("Float: " + floatNumber);
+         System.out.println("Double: " + doubleNumber);
+         System.out.println("Char: " + letter);
+         System.out.println("Unicode Char: " + unicodeChar);
+         System.out.println("String: " + greeting);
+         System.out.println("Boolean: " + isJavaFun);
+         System.out.println("Null: " + str);
 
     }
 }
