b0f871c1669dfb8f11fdc7b046f43d6e68811566
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-20 12:57:57 +0530

    update

--- Hello.java
+++ Hello.java
@@ -16,40 +16,49 @@ class Hello {
         // boolean num3 = true; 
 
          // Integer Literals
-         int decimal = 100;
-         int hex = 0x64;
-         int octal = 0144;
-         int binary = 0b1100100;
+        //  int decimal = 100;
+        //  int hex = 0x64;
+        //  int octal = 0144;
+        //  int binary = 0b1100100;
          
-         // Floating-Point Literals
-         float floatNumber = 3.14f;
-         double doubleNumber = 2.71828;
+        //  // Floating-Point Literals
+        //  float floatNumber = 3.14f;
+        //  double doubleNumber = 2.71828;
          
-         // Character Literals
-         char letter = 'A';
-         char unicodeChar = '\u0041';
+        //  // Character Literals
+        //  char letter = 'A';
+        //  char unicodeChar = '\u0041';
          
-         // String Literal
-         String greeting = "Hello, World!";
+        //  // String Literal
+        //  String greeting = "Hello, World!";
          
-         // Boolean Literal
-         boolean isJavaFun = true;
+        //  // Boolean Literal
+        //  boolean isJavaFun = true;
          
-         // Null Literal
-         String str = null;
+        //  // Null Literal
+        //  String str = null;
          
-         // Printing literals
-         System.out.println("Decimal: " + decimal);
-         System.out.println("Hexadecimal: " + hex);
-         System.out.println("Octal: " + octal);
-         System.out.println("Binary: " + binary);
-         System.out.println("Float: " + floatNumber);
-         System.out.println("Double: " + doubleNumber);
-         System.out.println("Char: " + letter);
-         System.out.println("Unicode Char: " + unicodeChar);
-         System.out.println("String: " + greeting);
-         System.out.println("Boolean: " + isJavaFun);
-         System.out.println("Null: " + str);
+        //  // Printing literals
+        //  System.out.println("Decimal: " + decimal);
+        //  System.out.println("Hexadecimal: " + hex);
+        //  System.out.println("Octal: " + octal);
+        //  System.out.println("Binary: " + binary);
+        //  System.out.println("Float: " + floatNumber);
+        //  System.out.println("Double: " + doubleNumber);
+        //  System.out.println("Char: " + letter);
+        //  System.out.println("Unicode Char: " + unicodeChar);
+        //  System.out.println("String: " + greeting);
+        //  System.out.println("Boolean: " + isJavaFun);
+        //  System.out.println("Null: " + str);
+
+        //Lecture 8 
+        //Conversion in Java & Casting 
+
+        float num1 = 3.5f;
+        int num2 = 345;
+
+        num2 = (int) num1;
+        System.out.println(num2);
 
     }
 }
