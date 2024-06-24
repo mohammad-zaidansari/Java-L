fe468efbea01e82f335fa00ce51f6350b31ab34f
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-24 23:10:13 +0530

    ifelse

--- if_else.java
+++ if_else.java
@@ -60,5 +60,26 @@ class if_else {
     //         System.out.println(c);
     //     }
 
+    //Write a Java program that takes a number from the user and generates an integer between 1 and 7. It displays the weekday name.
+
+    // Scanner input = new Scanner(System.in);
+    // System.out.print("Enter Day Number 1 to 7: ");
+    // int day = input.nextInt();
+
+    // if (day == 1)
+    // System.out.println("Monday");
+    // else if (day == 2)
+    // System.out.println("Tuseday");
+    // else if (day == 3)
+    // System.out.println("Wednesday");
+    // else if (day == 4)
+    // System.out.println("Thursday");
+    // else if (day == 5)
+    // System.out.println("Friday");
+    // else if (day == 6)
+    // System.out.println("Saturday");
+    // else if (day == 7)
+    // System.out.println("Sunday");
+
     }
 }
---
+++ ternary.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class ternary {
+    public static void main(String[] args) {
+        
+        // int n = 10;
+        // int result = 0;
+
+        // if(n%2==0){
+        //     result = 10;
+        // }
+        // else{
+        //     result = 20;
+        // }
+
+        // IN TERNARY OPERATER 
+        // result = ( n%2==0) ?  10 : 20;
+
+        // System.out.println(result);
+
+
+        // WAP to check whether odd or even number 
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        if (num%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+
+
+    }
+}
