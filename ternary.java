4d3878229c887c6ef3c784aa9bc732e27f9484ee
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-24 23:12:54 +0530

    new

--- ternary.java
+++ ternary.java
@@ -21,16 +21,16 @@ class ternary {
 
         // WAP to check whether odd or even number 
 
-        Scanner input = new Scanner(System.in);
+        // Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int num = input.nextInt();
+        // System.out.print("Enter a number: ");
+        // int num = input.nextInt();
 
-        if (num%2==0){
-            System.out.println("Even number");
-        }else{
-            System.out.println("Odd number");
-        }
+        // if (num%2==0){
+        //     System.out.println("Even number");
+        // }else{
+        //     System.out.println("Odd number");
+        // }
 
 
     }
