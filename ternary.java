cd4698d065633e4bbe5e8701173a14da9fb3e324
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-26 23:45:32 +0530

    new

--- ternary.java
+++ ternary.java
@@ -2,20 +2,20 @@
 class ternary {
     public static void main(String[] args) {
         
-        int n = 10;
-        int result = 0;
+        // int n = 10;
+        // int result = 0;
 
-        if(n%2==0){
-            result = 10;
-        }
-        else{
-            result = 20;
-        }
+        // if(n%2==0){
+        //     result = 10;
+        // }
+        // else{
+        //     result = 20;
+        // }
 
         // IN TERNARY OPERATER 
-        result = ( n%2==0) ?  10 : 20;
+        // result = ( n%2==0) ?  10 : 20;
 
-        System.out.println(result);
+        // System.out.println(result);
 
 
         // WAP to check whether odd or even number 
